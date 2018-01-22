29371111783a67946b4f52622718e798779d2a82
turpure <p812816627@163.com>
2018-01-22 15:18:07 +0800

    add moives components'

---
+++ src/components/Movies.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="movies">
+    <div slot="header" class="clearfix">
+      <h1 style="line-height: 36px; color: #20A0FF">豆瓣电影排行榜</h1>
+    </div>
+    <ul>
+      <li v-for="article in articles">
+        {{article.title}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        articles: []
+      }
+    },
+    mounted: function () {
+      this.$http.jsonp('https://api.douban.com/v2/movie/top250?count=100', {}, {
+        headers: {
+        },
+        emulateJSON: true
+      }).then(function (response) {
+        // 这里是处理正确的回调
+        this.articles = response.data.subjects
+        // this.articles = response.data["subjects"] 也可以
+      }, function (response) {
+        // 这里是处理错误的回调
+        console.log(response)
+      });
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import MyHello from '@/components/MyHello'
+import HelloWorld from '@/components/HelloWorld'
+import Movies from '@/components/Movies'
 
 Vue.use(Router)
 
 export default new Router({
+  // mode: 'history',
+  // hashbang: false,
   routes: [
     {
-      path: '/',
+      path: '/hello',
       name: 'MyHello',
       component: MyHello
-    }
+    },
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/movies',
+      name: 'Movies',
+      component:Movies
+    },
   ]
 })
